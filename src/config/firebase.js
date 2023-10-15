7df5ec7b9cf504c9da60c9eb0c09b13f2fb6fd45
Jordon Marchesano <jordonomarchesano@gmail.com>
2023-10-15 12:18:02 -0700

    revert 1

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,14 +7,15 @@ import {getAuth, GoogleAuthProvider} from 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "****************************",
-  authDomain: "****************************",
-  projectId: "****************************",
-  storageBucket: "****************************",
-  messagingSenderId: "****************************",
-  appId: "****************************",
-  measurementId: "****************************",
-}
+  apiKey: "AIzaSyBQ9XrbFE4DlRammWCNLP82otL-owTfcQY",
+  authDomain: "ts-fb-comicshop.firebaseapp.com",
+  projectId: "ts-fb-comicshop",
+  storageBucket: "ts-fb-comicshop.appspot.com",
+  messagingSenderId: "162802484457",
+  appId: "1:162802484457:web:74c52a615ac873f1801c78",
+  measurementId: "G-WKMJ73TN17"
+};
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth =  getAuth(app);
